a0baff25e2dc1724a70f6cd6db5ea5a781e944fe
wasifraz <iswasifraza@gmail.com>
2025-10-23 13:12:12 +0530

    project completed

--- main.py
+++ main.py
@@ -5,6 +5,13 @@ from student import Student
 from train import Train
 from face_recognition import Face_Recognition
 import os
+from time import strftime
+from datetime import datetime
+from tkinter import messagebox
+from attendance import Attendance
+from chatbot import UltimateGroqChatbot
+from help_desk import HelpDesk
+from tkinter import messagebox
 
 
 class Face_Recognition_System:
@@ -12,6 +19,8 @@ class Face_Recognition_System:
         self.root=root
         self.root.geometry("1530x790+0+0")
         self.root.title("Face Recognition System")
+        
+        
 
         # first image
         img=Image.open(r"college images\image1.jpg")
@@ -48,8 +57,13 @@ class Face_Recognition_System:
         bg_img=Label(self.root,image=self.photoimg3)
         bg_img.place(x=0,y=130,width=1530,height=710)
 
-        title_lbl=Label(bg_img,text="FACE RECOGNITION ATTENDANCE SYSTEM SOFTWARE",font=("times new roman",35,"bold"),bg="skyblue",fg="red")
+        title_lbl=Label(bg_img,text="FACE RECOGNITION ATTENDANCE SYSTEM SOFTWARE",font=("times new roman",30,"bold"),bg="skyblue",fg="red")
         title_lbl.place(x=0,y=0,width=1530,height=45)
+        
+        # ============================time=============================
+        self.time_lbl = Label(title_lbl, font=("times new roman", 14, "bold"), bg="skyblue", fg="white")
+        self.time_lbl.place(x=0, y=0, width=130, height=45) 
+        self.update_time()
 
         # student button
         img4=Image.open(r"college images\students.png")
@@ -78,21 +92,21 @@ class Face_Recognition_System:
         img6=img6.resize((220,220),Image.Resampling.LANCZOS)
         self.photoimg6=ImageTk.PhotoImage(img6)
 
-        b1=Button(bg_img,image=self.photoimg6,cursor="hand2")
+        b1=Button(bg_img,image=self.photoimg6,cursor="hand2",command=self.attendance)
         b1.place(x=800,y=100,width=220,height=220)
 
-        b1=Button(bg_img,text="Attendance",cursor="hand2",font=("times new roman",15,"bold"),bg="lightblue",fg="navy")
+        b1=Button(bg_img,text="Attendance",cursor="hand2",command=self.attendance,font=("times new roman",15,"bold"),bg="lightblue",fg="navy")
         b1.place(x=800,y=300,width=220,height=40)
 
-        # help desk button
-        img7=Image.open(r"college images\help desk.png")
+        #Chatbot button
+        img7=Image.open(r"college images\image2.png")
         img7=img7.resize((220,220),Image.Resampling.LANCZOS)
         self.photoimg7=ImageTk.PhotoImage(img7)
 
-        b1=Button(bg_img,image=self.photoimg7,cursor="hand2")
+        b1=Button(bg_img,image=self.photoimg7,cursor="hand2",command=self.chatbot)
         b1.place(x=1100,y=100,width=220,height=220)
 
-        b1=Button(bg_img,text="Help Desk",cursor="hand2",font=("times new roman",15,"bold"),bg="lightblue",fg="navy")
+        b1=Button(bg_img,text="Chatbot",cursor="hand2",command=self.chatbot,font=("times new roman",15,"bold"),bg="lightblue",fg="navy")
         b1.place(x=1100,y=300,width=220,height=40)
 
         # train button
@@ -117,15 +131,15 @@ class Face_Recognition_System:
         b1=Button(bg_img,text="Photos",cursor="hand2",command=self.open_img,font=("times new roman",15,"bold"),bg="lightblue",fg="navy")
         b1.place(x=500,y=580,width=220,height=40)
 
-        # developer button
+        # Help desk button
         img10=Image.open(r"college images\help desk.png")
         img10=img10.resize((220,220),Image.Resampling.LANCZOS)
         self.photoimg10=ImageTk.PhotoImage(img10)
 
-        b1=Button(bg_img,image=self.photoimg10,cursor="hand2")
+        b1=Button(bg_img,image=self.photoimg10,cursor="hand2",command=self.help_desk)   
         b1.place(x=800,y=380,width=220,height=220)
 
-        b1=Button(bg_img,text="Developer Details",cursor="hand2",font=("times new roman",15,"bold"),bg="lightblue",fg="navy")
+        b1=Button(bg_img,text="Help Desk",cursor="hand2",command=self.help_desk,font=("times new roman",15,"bold"),bg="lightblue",fg="navy")
         b1.place(x=800,y=580,width=220,height=40)
 
         # exit button
@@ -133,18 +147,23 @@ class Face_Recognition_System:
         img11=img11.resize((220,220),Image.Resampling.LANCZOS)
         self.photoimg11=ImageTk.PhotoImage(img11)
 
-        b1=Button(bg_img,image=self.photoimg11,cursor="hand2")
+        b1=Button(bg_img,image=self.photoimg11,cursor="hand2",command=self.exit)
         b1.place(x=1100,y=380,width=220,height=220)
 
-        b1=Button(bg_img,text="Exit",cursor="hand2",font=("times new roman",15,"bold"),bg="lightblue",fg="navy")
+        b1=Button(bg_img,text="Exit",cursor="hand2",command=self.exit,font=("times new roman",15,"bold"),bg="lightblue",fg="navy")
         b1.place(x=1100,y=580,width=220,height=40)
 
     def open_img(self):
         os.startfile("data")
-
-
-
-        # ============================Function buttons=============================
+        
+    def exit(self):
+        self.exit=messagebox.askyesno("Face Recognition","Are you sure to exit this project",parent=self.root)
+        if self.exit>0:
+            self.root.destroy()
+        else:
+            return
+            
+# ============================Function buttons=============================
     
     def student_details(self):
         self.new_window=Toplevel(self.root)
@@ -159,7 +178,28 @@ class Face_Recognition_System:
         self.new_window=Toplevel(self.root)
         self.app=Face_Recognition(self.new_window)
         
+    def attendance(self):
+        self.new_window=Toplevel(self.root)
+        self.app=Attendance(self.new_window)
+        
+    def help_desk(self):
+        self.new_window=Toplevel(self.root)
+        self.app=HelpDesk(self.new_window)
+    
+    def chatbot(self):
+        self.new_window=Toplevel(self.root)
+        self.app=UltimateGroqChatbot(self.new_window)
+        
+    def update_time(self):
+        """Fetches the current time and updates the time label."""
+        string = strftime('%H:%M:%S %p')
+        self.time_lbl.config(text=string)
+        # Schedule this method to run again after 1000ms (1 second)
+        self.time_lbl.after(1000, self.update_time)
+        
+        
 if __name__ == "__main__":
     root=Tk()
     obj=Face_Recognition_System(root)
     root.mainloop()
+    
